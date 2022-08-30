a497757909afce789de9a545e9c85561d5b3a35f
vanisan <amvan0202@gmail.com>
2022-08-30 14:45:04 +0300

    1.2

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-import os
-import telebot
-import logging
-from config *
-from flask import flask, request
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import types
+from aiogram.utils import executor
 
-bot = telebot.TeleBot (BOT_TOKEN)
-server = flask(__name__)
-logger = telebot.logger
-logger.setLevel(logging.DEBUG)
+bot = Bot(token='5421165424:AAGqJ8qtmaRap3kpLr0KpJl7jAiTU-SZjRQ')
 
-@bot.message_handler(commands=[start])
-def start(message):
-    username = message.from_user.username
-    bot_reply_to(message, f"Hello {username}")
+dp = Dispatcher(bot)
 
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+@dp.message_handler()
+async def get_message(message : types.Message):
+
+    chat_id = message.chat.id
+    text = f'Hello, {message.from_user.full_name}'
+
+    await bot.send_message(chat_id=chat_id, text=text)
 
 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0", port=int(os.eviron.get("PORT", 5000 )))
+
+executor.start_polling(dp)
